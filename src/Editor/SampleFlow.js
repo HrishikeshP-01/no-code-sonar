313ae2b5945020def6508ab9f45f7e0e525ac49b
Hrishikesh P <HP6043@slb.com>
2025-05-27 00:02:46 +0530

    Adding simple nodes

--- src/Editor/SampleFlow.js
+++ src/Editor/SampleFlow.js
@@ -1,10 +1,18 @@
 import { ReactFlow, Background, Controls } from '@xyflow/react'
 import '@xyflow/react/dist/style.css'
 
+const nodes = [
+    {
+        id: '1',
+        position: { x: 0, y: 0 },
+        data: { 'label':'Hello'}
+    }
+]
+
 function Flow() {
     return (
         <div style={{ height: '100vh', width: '100vw' }}> {/*Parent container of ReactFlows need a height & width specified. React flow uses this*/}
-            <ReactFlow>
+            <ReactFlow nodes={nodes}>
                 <h1>Hello</h1>
                 <Background />
                 <Controls />
